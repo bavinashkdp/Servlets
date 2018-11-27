401ca1549cb9674f6e35b7389b9cf2b72d7ed82b
user <user@user-PC>
2018-11-27 20:09:02 +0530

    second commit
    Cookie_Project
    
    all files are uploaded....

---
+++ Cookie_Project/src/org/jspiders/javApp/SecondServlet.java
@@ -0,0 +1,26 @@
+package org.jspiders.javApp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/ss")
+public class SecondServlet extends HttpServlet
+{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Cookie[]  arr=req.getCookies();
+		PrintWriter printWriter=resp.getWriter();
+		printWriter.println("<html><body bgcolor='pink'>"
+				+ "<h1 align='center'>"+arr[0].getValue()+"</h1>"
+				+ "</body></html>");
+		printWriter.flush();
+		printWriter.close();
+	}
+	
+}
